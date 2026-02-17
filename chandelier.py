e8bcaaf100fea4b3decdfe8fc37d3c0b8669ae4a
Brandon Dutcher <brandonbdutcher@gmail.com>
2026-02-16 23:19:42 -0800

    Remove scale code. Use inches directly

--- chandelier.py
+++ chandelier.py
@@ -14,12 +14,10 @@ special_dict = {
     "k": 4,  # kp
     "i": 5,  # ki
     "d": 6,  # kd
-    "o": 7,  # kp_pos (ramp rate when close, divide by 10000)
-    "x": 8,  # max speed (divide by 10)
+    "o": 7,  # kp_pos (ramp rate when close)
+    "x": 8,  # max speed (in/s divide by 10)
     "z": 9,  # command zero
 }
-
-DEFAULT_SCALE: int = 5
 
 
 def constrain(val: int, small: int, large: int) -> int:
@@ -28,19 +26,16 @@ def constrain(val: int, small: int, large: int) -> int:
 
 @final
 class Bulb:
-    def __init__(self, device: I2CDevice, scale: int = DEFAULT_SCALE):
+    def __init__(self, device: I2CDevice):
         self.device = device
-        if not 0 <= scale <= 16:
-            raise ValueError("Invalid scale, must be between 0 and 16")
-        self.scale = scale
         
     def zero(self):
         data = [0x00, 0x00, 0x09, 0x00]
         self.device.write(bytes(data))
 
-    def set_position(self, position: int):
-        # 16-bit max but with scale extra bits of range, scaled by those extra bits.
-        position = constrain(position, 0, ~(~0xFFFF << self.scale)) >> self.scale
+    def set_position(self, position: float):
+        # 16-bits: 1 byte for inches, 1 byte for fractions of an inch
+        position = constrain(int(position * 256), 0, 0xFFFF)
         data = position.to_bytes(2, byteorder="big") + bytes(2)
         self.device.write(data)
 
@@ -58,14 +53,14 @@ def driver(
         t = time.monotonic()
         for position, bulb in bulbs.items():
             extension = extensions(position.x, position.y, t)
-            bulb.set_position(int(extension))
+            bulb.set_position(extension)
         time.sleep(0.04)
 
 
 def main():
     try:
         bulbs = {position: Bulb(device) for position, device in i2c.get_all().items()}
-        driver(bulbs, lambda x, _, t: 3000 * sin(0.25 * x + 0.25 * t) + 3100)
+        driver(bulbs, lambda x, _, t: 3 * sin(0.25 * x + 0.25 * t) + 3.1)
     except KeyboardInterrupt:
         return
     except Exception as e:
