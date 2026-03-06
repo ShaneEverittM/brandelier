a33c9587dba2f478c301e13e02463df342c874ca
calvingolas <calvingolas@gmail.com>
2026-03-06 01:27:19 -0700

    added some init buttons

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from html import escape
+
+from flask import Flask, render_template, request
+
+import chandelier
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, Bran!</p>"
+    return render_template("index.html")
+
+@app.route("/button-action", methods=['GET', 'POST'])
+def button_action():
+    button_val = request.form.get('btn')
+    match button_val:
+        case "start":
+            chandelier.main()
+            return "<p>Congrats, the chandelier should have started</p>"
+        case "stop":
+            return "<h1>Things have been set into motion that can't be stopped</h1>"
+        case _:
+            return f"<p>Somehow you clicked a button that doesn't match the expected values: {button_val}</p>"
